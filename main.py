7d7bb85db921a15c6d991fc36a4ff485f0fc15b0
ZEROSAMA8 <113395068+ZEROSAMA8@users.noreply.github.com>
2022-09-16 11:47:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# #Data Types 
+# # print("Hello"[4])
+# # print(3 *( 3 + 3) / 3 - 3 )
+# # 🚨 Don't change the code below 👇
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+# inheight= float(height)
+# inweight=int(weight)
+
+# result = inweight / (inheight ** 2 ) 
+# print(int(result))
+
+
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇 
+agein= int(age)
+agein-= 90
+
+day = (agein * 365)
+week= (agein * 52)
+month=(agein *12)
+print( f"you have,{day} days ,{week} weeks , {month} months left ")
+
+
